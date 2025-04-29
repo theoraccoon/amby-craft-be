e27074ecfdf771d024bb93449da45a69fd5ff1c4
EdRaccoon <donatus.ankah@coderaccoon.com>
2025-04-29 10:06:44 +0000

    feat(auth): implement user authentication and signup with google auth

---
+++ src/auth/google/services/google-auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { ValidateOAuthLoginCommand } from '@google/commands/validate-oauth-login.command';
+
+interface GoogleUserInfo {
+    googleId: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    picture: string;
+    accessToken: string;
+    refreshToken: string;
+}
+
+@Injectable()
+export class GoogleAuthService {
+    constructor(private readonly commandBus: CommandBus) {}
+
+    async validateOAuthLogin(userInfo: GoogleUserInfo) {
+        const command = new ValidateOAuthLoginCommand(
+            userInfo.googleId,
+            userInfo.email,
+            userInfo.firstName,
+            userInfo.lastName,
+            userInfo.picture,
+            userInfo.accessToken,
+            userInfo.refreshToken
+        );
+
+        return this.commandBus.execute(command);
+    }
+}
