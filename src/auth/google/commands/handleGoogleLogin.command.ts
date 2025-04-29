ad65345b3fc9a939531d4045b0aefb4ee05ffb7e
EdRaccoon <donatus.ankah@coderaccoon.com>
2025-04-29 14:43:14 +0000

    feat(google-auth): implemented validate oauth login command

--- src/auth/google/commands/handleGoogleLogin.command.ts
+++
@@ -1,9 +0,0 @@
-export class HandleGoogleLoginCommand {
-  constructor(
-      public readonly googleId: string,
-      public readonly email: string,
-      public readonly firstName: string,
-      public readonly lastName: string,
-      public readonly profilePicture: string
-  ) {}
-}
