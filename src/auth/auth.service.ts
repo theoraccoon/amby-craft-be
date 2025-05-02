a3cbb8ab2344c07b0d2165fd86f531fb2fa89b8f
BornT0CodeRaccoon <bernard.appiah@coderaccoon.com>
2025-05-02 10:26:10 +0000

    chore: reduce lines of codes pushed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
 import { UsersService } from 'src/users/users.service';
-import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly userService: UsersService,
-    private readonly databaseService: PrismaService,
-    private readonly jwtService: JwtService,
-    private readonly configService: ConfigService,
-  ) {}
+  constructor(private readonly userService: UsersService) {}
 
   // Method to validate user credentials and create a new user
   async validateUser(body: CreateUserDto) {
