d695b33948d9de0f60c18eb3be81c2b590dc002c
EdRaccoon <donatus.ankah@coderaccoon.com>
2025-05-13 08:53:04 +0000

    feat(decorator-authguard): implement-decorator-current-user-and-auth-guard

--- src/modules/course/queries/handlers/get-course-by-id.handler.ts
+++ src/modules/course/queries/handlers/get-course-by-id.handler.ts
@@ -3,21 +3,19 @@ import { Prisma } from '@prisma/client';
 import { GetCourseQuery } from '../get-course-by-id.query';
 import { DatabaseService } from '@common/database/database.service';
 
-type CourseWithLessonsAndBlocks = Prisma.CourseGetPayload<{
-  include: {
-    lessons: {
-      include: {
-        blocks: true;
-      };
-    };
-  };
-}>;
-
 @QueryHandler(GetCourseQuery)
 export class GetCourseHandler implements IQueryHandler<GetCourseQuery> {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async execute(query: GetCourseQuery): Promise<CourseWithLessonsAndBlocks | null> {
+  async execute(query: GetCourseQuery): Promise<Prisma.CourseGetPayload<{
+    include: {
+      lessons: {
+        include: {
+          blocks: true;
+        };
+      };
+    };
+  }> | null> {
     return this.databaseService.course.findUnique({
       where: { id: query.courseId },
       include: {
