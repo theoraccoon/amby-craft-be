ecff1cea718760126f6b78cb76719b28be7a885d
SammyRaccoon <samuel.obugyei@coderaccoon.com>
2025-05-19 14:14:28 +0000

    feat(course): added route to delete functionality for course module

--- src/modules/course/courses.controller.ts
+++ src/modules/course/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param, UseGuards, Patch } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, UseGuards, Patch, Delete } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateCourseCommand } from './commands/create-course.command';
 import { GetCourseQuery } from './queries/get-course-by-id.query';
@@ -8,6 +8,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { CurrentUser } from '@common/decorators/current-user.decorator';
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { UpdateCourseCommand } from './commands/update-course.command';
+import { DeleteCourseCommand } from './commands/delete-course.command';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('courses')
@@ -45,5 +46,10 @@ export class CoursesController {
   @Patch(':id')
   async updateCourse(@Param('id') courseId: string, @CurrentUser('userId') userId: string, @Body() updateData: UpdateCourseDto) {
     return this.commandBus.execute(new UpdateCourseCommand(courseId, userId, updateData));
+  }
+
+  @Delete(':id')
+  async deleteCourse(@Param('id') courseId: string, @CurrentUser('userId') userId: string) {
+    return this.commandBus.execute(new DeleteCourseCommand(courseId, userId));
   }
 }
